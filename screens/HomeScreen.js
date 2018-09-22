ed106ce931048d07d0265df634a014dc618aed03
ginpei <ginpei@gmail.com>
2018-09-22 16:55:31 -0700

    try button

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
+
+  constructor (props) {
+    super(props);
+    this.onTryPress = this.onTryPress.bind(this);
+  }
 
   render() {
     return (
@@ -14,8 +19,19 @@ export default class HomeScreen extends React.Component {
           style={styles.topKaiunImage}
           source={require('../assets/images/kaiun.png')}/>
         <Text style={styles.topKaiunText}>今日の運勢は？</Text>
+        <Image
+          style={styles.daikichiImage}
+          source={require('../assets/images/daikichi.png')}
+          />
+        <TouchableOpacity style={styles.tryButton} onPress={this.onTryPress}>
+          <Text style={styles.tryButtonText}>おみくじを引く</Text>
+        </TouchableOpacity>
       </View>
     );
+  }
+
+  onTryPress () {
+    alert('大吉！');
   }
 }
 
@@ -34,5 +50,17 @@ const styles = StyleSheet.create({
   },
   topKaiunText: {
     fontSize: 40,
+  },
+  daikichiImage: {
+    resizeMode: 'contain',
+    height: 300,
+    width: 200,
+  },
+  tryButton: {
+    backgroundColor: '#036',
+    padding: 8,
+  },
+  tryButtonText: {
+    color: 'white',
   },
 });
